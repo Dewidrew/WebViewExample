b308b5c32efeab2ce1980ad8934c0e3bcd2009e1
Hattapong <Hattapong.M@augmentis.biz>
2016-09-07 13:41:03 +0700

    WebView

---
+++ app/src/main/java/ayp/aug/examplewebview/MainActivity.java
@@ -0,0 +1,27 @@
+package ayp.aug.examplewebview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView)findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient(){
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return false;
+            }
+        });
+        webView.loadUrl("http://www.google.com/");
+    }
+}
